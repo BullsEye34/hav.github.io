1d2641f8dfa539ed038101a75debdbf40e273962
vamshi <vamvijkum@gmail.com>
2019-01-19 22:11:46 +0530

    Bring it back to normal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, edge);
+$(document).ready(function(){
+  $('.sidenav').sidenav();
 });
 
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
   });
+});
